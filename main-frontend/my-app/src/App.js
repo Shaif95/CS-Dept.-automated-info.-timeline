d7b0445d189f79172a7ea2b9620994af56d9af31
shaif95 <chowdhuryshaif@rediffmail.com>
2021-10-11 21:21:47 -0500

    clean

--- main-frontend/my-app/src/App.js
+++ main-frontend/my-app/src/App.js
@@ -2,29 +2,87 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-
+import './res/index.css'
 import Popper from 'popper.js';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+    
+     <body>
+
+
+<nav class="navbar navbar-expand-sm navbar-dark scrolling-navbar">
+
+
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+ <ul class="navbar-nav">
+<li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+
+<li class="nav-item">
+      <a class="nav-link" href="#">Events</a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="#">Grants</a>
+    </li>
+
+
+<li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Login
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Admin Login</a>
+        <a class="dropdown-item" href="#">Faculty Login</a>
+      </div>
+    </li>
+
+ </ul>
+
+
     </div>
+
+
+
+</nav>
+
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+
+ 
+  
+
+</div>
+
+
+
+   
+
+   
+  </body>
+   
+
+
+
+
+
   );
 }
 
