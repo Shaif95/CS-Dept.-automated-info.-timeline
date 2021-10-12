0997b74956edb3fa1eaa7e09b38781a3a971cf8b
shaif95 <chowdhuryshaif@rediffmail.com>
2021-10-11 21:14:42 -0500

    adding home files

--- main-frontend/my-app/src/App.js
+++ main-frontend/my-app/src/App.js
@@ -1,3 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+
+import Popper from 'popper.js';
 import logo from './logo.svg';
 import './App.css';
 
