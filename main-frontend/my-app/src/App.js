9be090b48ae6c382583f0c6370d9f85e80d4afbe
shaif95 <chowdhuryshaif@rediffmail.com>
2021-10-11 21:51:58 -0500

    adding homepage

--- main-frontend/my-app/src/App.js
+++ main-frontend/my-app/src/App.js
@@ -1,87 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import './res/index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-import './res/index.css'
 import Popper from 'popper.js';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
 
 
-
-    
-     <body>
-
-
-<nav class="navbar navbar-expand-sm navbar-dark scrolling-navbar">
-
-
-
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-  
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
- <ul class="navbar-nav">
-<li class="nav-item">
-      <a class="nav-link" href="#">Home</a>
-    </li>
-
-<li class="nav-item">
-      <a class="nav-link" href="#">Events</a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link" href="#">Grants</a>
-    </li>
-
-
-<li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Login
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Admin Login</a>
-        <a class="dropdown-item" href="#">Faculty Login</a>
-      </div>
-    </li>
-
- </ul>
-
-
-    </div>
-
-
-
-</nav>
-
-
-
-
-<div id="demo" class="carousel slide" data-ride="carousel">
-
-
- 
-  
-
-</div>
-
-
-
+  <div>
+  </div>
    
-
-   
-  </body>
-   
-
-
-
-
 
   );
 }
--- main-frontend/my-app/src/index.js
+++ main-frontend/my-app/src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import $ from 'jquery';
 import Popper from 'popper.js';
 import React from 'react';
